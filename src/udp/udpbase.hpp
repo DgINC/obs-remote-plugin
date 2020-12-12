82b8cd5b9cdec1d7317b22ae46068d8af7eb124c
Leonid Kuzin (aka DgINC) <dg.inc.lcf@gmail.com>
2020-12-12 10:52:26 +0300

    Initial commit

---
+++ src/udp/udpbase.hpp
@@ -0,0 +1,21 @@
+/*
+ * udpbase.hpp
+ *
+ *  Created on: 12 дек. 2020 г.
+ *      Author: dg
+ */
+
+#ifndef SRC_UDP_UDPBASE_HPP_
+#define SRC_UDP_UDPBASE_HPP_
+
+namespace UDPHandShake {
+
+class udp_base {
+public:
+	udp_base();
+	virtual ~udp_base();
+};
+
+} /* namespace UDPHandShake */
+
+#endif /* SRC_UDP_UDPBASE_HPP_ */
---
+++ src/udp/udpserver.hpp
@@ -0,0 +1,21 @@
+/*
+ * udpserver.hpp
+ *
+ *  Created on: 12 дек. 2020 г.
+ *      Author: dg
+ */
+
+#ifndef SRC_UDP_UDPSERVER_HPP_
+#define SRC_UDP_UDPSERVER_HPP_
+
+namespace UDPHandShake {
+
+class udp_server {
+public:
+	udp_server();
+	virtual ~udp_server();
+};
+
+} /* namespace UDPHandShake */
+
+#endif /* SRC_UDP_UDPSERVER_HPP_ */
